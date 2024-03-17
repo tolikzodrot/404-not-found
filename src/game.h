e7e22044ba0b6348695dde0781a1784925e82955
liuxia <3402040030@qq.com>
2024-03-17 18:38:36 +0800

    code comment

--- src/game.h
+++ src/game.h
@@ -13,8 +13,8 @@ public:
     Game();
     ~Game();
 
-    bool init();
-    void run();
+    bool init();//initialize the function
+    void run();//run the game
 
 private:
     SDL_Window* window = nullptr;
@@ -22,21 +22,21 @@ private:
 
     Textures* textures = nullptr;
 
-    const float FPS = 60.0;
+    const float FPS = 60.0;//define the game's target frame rate
     int SCREEN_WIDTH;
-    int SCREEN_HEIGHT;
+    int SCREEN_HEIGHT;//the width and height of the screen
 
     int MAP_WIDTH = 15;
-    int MAP_HEIGHT = 6;
+    int MAP_HEIGHT = 6;//the width and height of the map
 
     bool running = true;
 
     Player* player = nullptr;
     Tilemap* tilemap = nullptr;
 
-    void input();
-    void update();
-    void render();
+    void input();//processing game input
+    void update();//uptate the game
+    void render();//render game screen
 };
 
 #endif // GAME.H
