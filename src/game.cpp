d012d3abb32f403f547651431c75311c297fca65
liuxia <3402040030@qq.com>
2024-03-31 03:15:10 +0800

    change menu code to game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,5 @@
 #include "game.h"
-
+SDL_Texture* menuBackground=nullptr;
 Game::Game() {
 }
 
@@ -13,9 +13,21 @@ Game::~Game() {
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    IMG_Quit();
+    SDL_DestroyTexture(menuBackground); 
     SDL_Quit();
+    IMG_Quit();
 }
+
+bool Game::loadMedia(SDL_Renderer* renderer) {  
+    const char* menuBackgroundPath= "assets/tiles/menu/menuBackgroundPath.png";
+    menuBackground = IMG_LoadTexture(renderer, menuBackgroundPath);  
+    if (menuBackground == NULL) {  
+        printf("Failed to load media!\n");  
+        return false;  
+    }  
+  
+    return true;  
+}  
 
 bool Game::init() {
     // Initialize SDL
@@ -24,11 +36,10 @@ bool Game::init() {
         return false;
     }
 
-
-    // Initialize SDL_image
     if (!(IMG_Init(IMG_INIT_TIF) & IMG_INIT_TIF)) {
         printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-        return 1;
+        SDL_Quit();
+        return false;
     }
 
     // Get the display mode of the primary monitor
@@ -36,20 +47,21 @@ bool Game::init() {
     if (SDL_GetCurrentDisplayMode(0, &display_mode) != 0) {
         // Handle getting display mode error
         printf("SDL could not get display dimensions! SDL_Error: %s\n", SDL_GetError());
+        IMG_Quit(); // Clean up SDL_image  
+        SDL_Quit(); // Clean up SDL  
         return false;
     }
-
-    SCREEN_WIDTH = display_mode.w;
-    SCREEN_HEIGHT = display_mode.h;
     SCREEN_WIDTH = 1200;
     SCREEN_HEIGHT = 720;
     MAP_WIDTH = 15;
     MAP_HEIGHT = 9;
 
     // Create the windows using the monitors dimensions (fullscreen) FOR RELEASE: SDL_WINDOW_FULLSCREEN
-    window = SDL_CreateWindow("The Depth of Eternity", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("The Depth of Eternity", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        IMG_Quit();
+        SDL_Quit();
         return false;
     }
 
@@ -57,10 +69,20 @@ bool Game::init() {
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (renderer == nullptr) {
         printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);  
+        IMG_Quit();  
+        SDL_Quit();  
+        return false;
+    }
+    // Load all the textures (exit if there is an error loading a texture)
+
+    if(!loadMedia(renderer)){
+        printf("Failed to load media!\n");  
+        IMG_Quit();  
+        SDL_Quit();  
         return false;
     }
 
-    // Load all the textures (exit if there is an error loading a texture)
     textures = new Textures(renderer);
     if (!textures->load()) {
         running = false;
@@ -82,24 +104,56 @@ void Game::run() {
     // Initialize the FPS handlers
     Uint32 frame_start;
     int frame_time;
-
-    while (running) {
-        // Get the current time
+    while (running) { 
+            // Get the current time
         frame_start = SDL_GetTicks();
-
-        // Handle the input
-        input();
+        switch(Gamestate){
+            case MENU:
+                handleMenuInput();
+                break;  
+            case PLAYING:
+                // Handle the input
+                input();
         // Update the game logic
-        update();
+                update();
         // Render
-        render();
-
-        // Control the frame rate
+                render();
+        }
+         // Control the frame rate
         frame_time = SDL_GetTicks() - frame_start;
         if (1000 / FPS > frame_time) {
             SDL_Delay(1000 / FPS - frame_time);
         }
+        
     }
+}
+
+void Game::handleMenuInput(){
+    SDL_Event event;
+    while (SDL_PollEvent(&event) != 0) {
+    	// If quit is detected
+        if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && (event.key.keysym.sym == SDLK_ESCAPE || event.key.keysym.sym == SDLK_q))) {
+            // Quit the game
+            running=false;
+        }
+        else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
+            if (isMouseInButton(&event, BUTTON_PLAY_X, BUTTON_PLAY_Y, BUTTON_PLAY_W, BUTTON_PLAY_H)) {
+                running=true;
+                Gamestate=PLAYING;
+            }
+            else if (isMouseInButton(&event, BUTTON_LOAD_X, BUTTON_LOAD_Y, BUTTON_LOAD_W, BUTTON_LOAD_H)) {
+                running=true;   
+                Gamestate=PLAYING; 
+            } else if (isMouseInButton(&event, BUTTON_EXIT_X, BUTTON_EXIT_Y, BUTTON_EXIT_W, BUTTON_EXIT_H)) {
+                running=false;
+            } 
+        } 
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);  
+        SDL_RenderClear(renderer);  
+        SDL_RenderCopy(renderer, menuBackground, NULL, NULL);  
+        SDL_RenderPresent(renderer); 
+        
+    }   
 }
 
 void Game::input() {
@@ -110,12 +164,9 @@ void Game::input() {
             // Quit the game
             running = false;
         }
-
         // Handle the input for the player
-
-
         player->handle_input(event);
-    }
+     }
 }
 
 void Game::update() {
@@ -129,9 +180,7 @@ void Game::update() {
 void Game::render() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
-
     // TEST FOR HITBOXES
-
     // Render the tilemap object
     tilemap->render();
     // Render the player object
@@ -139,3 +188,13 @@ void Game::render() {
 
     SDL_RenderPresent(renderer);
 }
+
+bool Game::isMouseInButton(SDL_Event* event, int x, int y, int w, int h) {  
+    int mouseX, mouseY;  
+    SDL_GetMouseState(&mouseX, &mouseY);  
+    return (mouseX >= x && mouseX <= x + w && mouseY >= y && mouseY <= y + h);  
+}  
+
+bool Game::isRunning() const {  
+    return running;  
+}  
--- src/game.h
+++ src/game.h
@@ -3,11 +3,18 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <stdbool.h>  
+#include <string>
 
 #include "textures.h"
 #include "player.h"
 #include "tilemap.h"
 #include "movement.h"
+
+enum Gamestate{
+    MENU,
+    PLAYING
+};
 
 class Game {
 public:
@@ -16,22 +23,41 @@ public:
 
     bool init();//initialize the function
     void run();//run the game
+    bool isMouseInButton(SDL_Event* event, int x, int y, int w, int h) ; 
+    bool isRunning() const;
+    void handleMenuInput();
+    bool loadMedia(SDL_Renderer* renderer);
+
 
 private:
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
-
     Textures* textures = nullptr;
+    SDL_Texture* menuBackground=nullptr;
 
     const float FPS = 60.0;//define the game's target frame rate
     int SCREEN_WIDTH;
     int SCREEN_HEIGHT;//the width and height of the screen
+    const char* menuBackgroundPath= "assets/tiles/menu/menuBackgroundPath.png";
+
+    const int  BUTTON_PLAY_X = 850;
+    const int BUTTON_PLAY_Y = 430;
+    const int  BUTTON_PLAY_W = 430;
+    const int BUTTON_PLAY_H = 105;
+    const int BUTTON_LOAD_X = 695;
+    const int BUTTON_LOAD_Y = 690;
+    const int  BUTTON_LOAD_W = 120 ;
+    const int  BUTTON_LOAD_H = 230;
+    const int BUTTON_EXIT_X = 955;
+    const int BUTTON_EXIT_Y = 565;
+    const int BUTTON_EXIT_W = 230;
+    const int BUTTON_EXIT_H = 120;
 
     int MAP_WIDTH;
     int MAP_HEIGHT;//the width and height of the map
 
     bool running = true;
-
+    bool  Gamestate = MENU;
     Player* player = nullptr;
     Tilemap* tilemap = nullptr;
 
